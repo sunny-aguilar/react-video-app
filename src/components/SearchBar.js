ba7b56b82cd989bc4a14e2ac2125aca46fc9d5a9
sunny-aguilar <sunny.aguilar@gmail.com>
2020-07-17 20:15:40 -0700

    react

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -6,7 +6,14 @@ class SearchBar extends React.Component {
   }
 
   render() {
-    return <div><h1>Search Bar</h1></div>;
+    return (
+      <div>
+        <h1>Search Bar</h1>
+        <form>
+          <input className="field" type="text" />
+        </form>
+      </div>
+    );
   }
 }
 
